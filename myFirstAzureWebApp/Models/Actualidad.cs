e76df405ffc41e6399472d90d966aa884b2a931e
santitriana1011 <41800013+santitriana1011@users.noreply.github.com>
2019-09-12 21:51:38 -0500

    cambios programacion

---
+++ myFirstAzureWebApp/Models/Actualidad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myFirstAzureWebApp.Models
+{
+    public class Actualidad
+    {
+        public int ActualidadID { get; set; }
+        [Required]
+        public String Nombre { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = false)]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public String Descripcion { get; set; }
+        
+    }
+}
